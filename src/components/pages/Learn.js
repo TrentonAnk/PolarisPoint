459e980dd25f268dc740ea34c0d821b04af3a4b9
PawelLyko <pawel_lyko@yahoo.com>
2018-08-09 13:19:33 -0400

    adding stuff

--- src/components/pages/Learn.js
+++ src/components/pages/Learn.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import API from "../../utils/API"
 
 
 
@@ -11,7 +11,16 @@ const canvasStyle = {
     marginRight: '18%'
 }
 
-
+function renderQuiz() {
+    API.getQuiz();
+    .then(res =>
+        this.setState({
+            books: res.data,
+            text: "",
+            category: ""
+        })
+    ).catch(err => console.log(err));
+}
 
 
 
@@ -23,6 +32,17 @@ const Learn = () => ( <
     p >
     People have long been struggling to learn new material, so we incorperated our favorite method of learning, and applied it to our website.Here you can track your progress, learn new material, and have fun doing so!Let 's get learning!!! < /
     p >
+
+
+
+    <
+    RenderBtn onClick = {
+        () => this.renderQuiz(questions.category)
+    }
+    />
+
+
+
 
     <
     canvas style = {
